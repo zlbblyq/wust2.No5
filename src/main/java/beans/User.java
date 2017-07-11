4bd9495529228e0188e44520610fe4186c8fef31
cutepigpig <Li Yuan@DESKTOP-G6RITT3>
2017-07-11 15:31:48 +0800

    update java

---
+++ src/main/java/beans/User.java
@@ -0,0 +1,27 @@
+package beans;
+
+public class User {
+	String id;
+	String username;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	String password;
+
+
+}
